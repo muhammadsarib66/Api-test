ee44229650238b77bf528e3209027e608332e2df
muhammadsarib66 <saribnoor0310@gmail.com>
2023-12-13 16:28:00 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,8 @@ app.get('/api/v1/getheroinfo',getHeroInfo)
 
 
 // blog ////  
-app.get('/', async (req, res) => {
-    await res.send('hello world');
+app.get('/',  (req, res) => {
+     res.json('hello world');
 });
 
 app.post('/api/v1/createblog',CreateBlog);
